26cc3bd824db3c4dcbc62e936c59f9b1860d9b05
林祐德 <qwerty239qwe@gmail.com>
2021-07-18 23:15:14 +0800

    complete UCell functions

--- scTenifold/cell_cycle/UCell.py
+++ scTenifold/cell_cycle/UCell.py
@@ -1,17 +1,23 @@
-import pandas as pd
-import numpy as np
 from functools import partial
 from warnings import warn
+from typing import Optional, List
+
+import pandas as pd
+import numpy as np
+
+from scTenifold.cell_cycle.utils import *
 
 
-def _check_features(df, features):
+def _check_features(df,
+                    features):
     valid_features = set(df.index) & set(features)
     if len(features) != len(valid_features):
         warn(f"Found {len(features) - len(valid_features)} invalid features (e.g. not shown in the dataframe)")
     return valid_features
 
 
-def calc_auc(rank_val: pd.Series, max_rank: int):
+def calc_auc(rank_val: pd.Series,
+             max_rank: int):
     insig_part = rank_val > max_rank
     if all(insig_part):
         return 0
@@ -19,12 +25,16 @@ def calc_auc(rank_val: pd.Series, max_rank: int):
         rank_val[insig_part] = max_rank + 1
         rank_sum = sum(rank_val)
         n = rank_val.shape[0]
-        u_val = rank_sum - (n * (n + 1)) / 2
-        auc = 1 - u_val / (n * max_rank)
+        u_val = rank_sum - (n * (n + 1)) / 2  # lower if the rank is higher
+        auc = 1 - (u_val / (n * max_rank))
         return auc
 
 
-def calc_U_stat_df(features, df: pd.DataFrame, neg_features=None, max_rank=1500, w_neg=1):
+def calc_U_stat_df(features,
+                   df: pd.DataFrame,
+                   neg_features: Optional[List[str]] = None,
+                   max_rank=1500,
+                   w_neg=1):
     if neg_features is None:
         neg_features = []
     pos_features = list(set(features) - set(neg_features))
@@ -38,16 +48,20 @@ def calc_U_stat_df(features, df: pd.DataFrame, neg_features=None, max_rank=1500,
     else:
         neg = np.zeros(shape=(df.shape[2],))
     diff = pos - w_neg * neg
-    diff[diff < 0] = 0
+    # diff[diff < 0] = 0
     return diff
 
 
-def calc_Uscore(df: pd.DataFrame,
-                features, neg_features, max_rank=1500, w_neg=1, ties_method="average"):
-    ranked_df = df.rank(ascending=False, method=ties_method)
-    features = _check_features(df, features)
-    cell_auc = calc_U_stat_df(features, df,
-                              neg_features=neg_features,
+def calc_Uscore(X: pd.DataFrame,
+                pos_genes,
+                neg_genes,
+                max_rank=1500,
+                w_neg=1,
+                ties_method="average"):
+    ranked_df = X.rank(ascending=False, method=ties_method)
+    pos_genes = _check_features(X, pos_genes)
+    cell_auc = calc_U_stat_df(pos_genes, ranked_df,
+                              neg_features=neg_genes,
                               max_rank=max_rank,
                               w_neg=w_neg)
-    return pd.DataFrame(cell_auc, columns=ranked_df.columns)
+    return pd.DataFrame(cell_auc, index=ranked_df.columns)
