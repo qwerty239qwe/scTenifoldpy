04c5703eff6002150895aeca85f532aab20b3478
yu-de lin <qwerty239qwe@gmail.com>
2021-12-07 12:15:29 +0800

    Use R's default setting

--- scTenifold/core/_networks.py
+++ scTenifold/core/_networks.py
@@ -294,9 +294,9 @@ def manifold_alignment(X: pd.DataFrame,
                          np.concatenate((w_XY.T, w_Y), axis=1)), axis=0)
     np.fill_diagonal(W, 0)
     np.fill_diagonal(W, -W.sum(axis=0))
-    eg_vals, eg_vecs = scipy.sparse.linalg.eigs(W, k=d * 2, which="SR")
+    eg_vals, eg_vecs = scipy.sparse.linalg.eigs(W, k=d * 2, which="SR", tol=1e-14)
     eg_vecs = eg_vecs[:, eg_vals >= tol]
-    eg_vecs = eg_vecs[:, np.argsort(eg_vals[eg_vals >= tol], )[::-1]]
+    eg_vecs = eg_vecs[:, np.argsort(eg_vals[eg_vals >= tol], )]
     return pd.DataFrame(eg_vecs[:, :d],
                         index=["X_{g}".format(g=g) for g in shared_genes]+["Y_{g}".format(g=g) for g in shared_genes],
                         columns=["NLMA_{i}".format(i=i+1) for i in range(min(d, eg_vecs.shape[1]))])
@@ -319,9 +319,10 @@ def d_regulation(data,
     ascending: bool or list of bool, default = True
         Sorted ascending (otherwise descending)
     **kwargs
-        Keyword arguments for statistic analyses,
+        Keyword arguments for statistic analyses, and n_ko_genes (if any)
             boxcox_kws - kwargs for boxcox test
             chi2_kws - kwargs for chi-square test
+            n_ko_genes - int, indicating the number of KO genes
 
     Examples
     ---------
@@ -355,7 +356,8 @@ def d_regulation(data,
         t_d_metrics = d_metrics
 
     z_scores = (t_d_metrics - t_d_metrics.mean()) / t_d_metrics.std()
-    expected_val = np.mean(np.power(d_metrics, 2))
+    n_ko_genes = kwargs.get("n_ko_genes") if "n_ko_genes" in kwargs else 0
+    expected_val = np.mean(np.power(d_metrics[np.argsort(d_metrics)[::-1][n_ko_genes:]], 2))
     FC = np.power(d_metrics, 2) / expected_val
     p_values = 1 - stats.chi2.cdf(FC, **chi2_kws)
     p_adj = cal_fdr(p_values)
